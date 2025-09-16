a761eb0d0fac701cef15820f3db065c5c45abe12
yogu-code <yogeshrane019@gmail.com>
2025-09-16 12:56:11 +0530

    refactor: simplify page component by removing unused imports and mobile navigation

--- src/app/generatequestionpaper/page.jsx
+++ src/app/generatequestionpaper/page.jsx
@@ -1,51 +1,10 @@
 
-import DesktopNavbar from "@/components/desktopnav/nav";
-import Sidebar from "@/components/desktopsidebar/sidebar";
-import MobilebottomNavbar from "@/components/mobilenav/MobileBottomNavbar";
-import MobileNavbar from "@/components/mobilenav/mobilenav";
+import React from 'react'
 import Paper from '@/components/generatequestionpaper/Paper'
-import PaperMobile from '@/components/generatequestionpaper/PaperMobile'
-
-
-
-const page = () => {
+export default function page() {
   return (
-    <>
-       {/* Desktop Sidebar Section (visible on md+) */}
-       <div className="md:w-1/6">
-        <Sidebar />
-      </div>
-      <DesktopNavbar/>
-
-      <main className="block md:hidden">
-
-     {/* Mobile Navs */}
-    <MobileNavbar/>
-
-    {/* Mobile Navs */}  
-    <MobilebottomNavbar/>
-
-   {/* Page Content */}
-
-
-  </main>
-
-
-
-
-
-
-   
-    <div className=' hidden md:block'>
-      <Paper/>
+    <div>
+      <Paper />
     </div>
-    <div className='md:hidden'>
-      <PaperMobile/>
-    </div>
-
-
-    </>
   )
 }
-
-export default page
