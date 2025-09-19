30564548ffe537a19f6a909389f988b12a056f71
yogu-code <yogeshrane019@gmail.com>
2025-09-19 16:26:07 +0530

    fix the mobile view issuse of this pages

--- src/app/test_preview/page.jsx
+++ src/app/test_preview/page.jsx
@@ -1,39 +1,42 @@
 "use client";
 
-//import MobileNavbar from "C:/Users/user/Desktop/EP_Admin_Frontend/src/components/moblieadminnav/nav.jsx";
-
 import React, { useState } from "react";
-//import Mobile_Office_mode from "@/components/Moblie/moblie_offline_mode";
 
 import MobilebottomNavbar from "@/components/mobilenav/MobileBottomNavbar";
 import Sidebar from "@/components/desktopsidebar/sidebar";
 import DesktopNavbar from "@/components/desktopnav/nav";
 import TestPreview from "@/components/Action_test_preview/Test_preview";
-import Moblie_test_Preview from "@/components/Moblie/moblie_test_preview";
 import MobileNavbar from "@/components/mobilenav/mobilenav";
 
 const Page = () => {
-  const [selectedMode, setSelectedMode] = useState("Practice"); // State to track selected mode
+  const [selectedMode, setSelectedMode] = useState("Practice");
 
   return (
     <div className="min-h-screen md:flex bg-white">
-      <div className="md:hidden block">
+      {/* Mobile View */}
+      <div className="md:hidden block relative">
         <MobileNavbar />
-        <Moblie_test_Preview />
-        <MobilebottomNavbar />
-      </div>
-
-      {/* Sidebar Section */}
-      <div className="md:w-1/6">
-        <Sidebar />
-      </div>
-      <div className="w-full md:w-5/6 md:flex-1 h-screen bg-white">
-        {/* Navigation Bar */}
-        <DesktopNavbar />
-
-        <main className="hidden md:block">
+        {/* add bottom padding so content is not hidden behind fixed bottom navbar */}
+        <div className="pb-16">
           <TestPreview />
-        </main>
+        </div>
+        <div className="fixed bottom-0 left-0 w-full">
+          <MobilebottomNavbar />
+        </div>
+      </div>
+
+      {/* Desktop View */}
+      <div className="hidden md:flex w-full">
+        {/* Sidebar Section */}
+        <div className="md:w-1/6">
+          <Sidebar />
+        </div>
+        <div className="w-full md:w-5/6 md:flex-1 h-screen bg-white">
+          <DesktopNavbar />
+          <main>
+            <TestPreview />
+          </main>
+        </div>
       </div>
     </div>
   );
